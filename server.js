2349bd87e96b0936d55403018c7b905c0686df4e
Aman <amanmatreja@gmail.com>
2022-01-10 17:47:00 +0530

    Made the full functionality - HAVE to clean the code

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const res = require('express/lib/response');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,19 +21,41 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
+
+// microservice code
+app.get('/api/', (req, res)=> {
+  return res.json({'unix': new Date().getTime(), 'utc': new Date().toUTCString()})
+})
+
+
 
 app.get('/api/:mydate', (req, res)=> {
   let date = req.params.mydate
+
   if (Number(date)) {
     let my_date = new Date(Number(date)).toUTCString()
-    res.json({'unix': Number(date), 'utc': my_date})
-  }else {
-    res.json({'unix': new Date(date).getTime(), 'utc': new Date(date).toUTCString()})
-  };
+    if (my_date === "Invalid Date"){
+      return res.json({'error': "Invalid Date"})
+    }
+    return res.json({'unix': Number(date), 'utc': my_date})
+
+    }else
+    {
+      let my_date = new Date(date).toUTCString()
+      if (my_date === "Invalid Date"){
+        return res.json({'error': "Invalid Date"})
+      }
+        return res.json({'unix': new Date(date).getTime(), 'utc': new Date(date).toUTCString()})
+  }
+
 })
+
+
+
+
 
 // listen for requests :)
 var listener = app.listen(5000, function () {
@@ -40,17 +63,5 @@ var listener = app.listen(5000, function () {
   console.log('server is listening on 5000');
 });
 
-// let ok = new Date('2022-10-15').getTime()
-// console.log(ok)
 
 
-
-// let final = 1665792000000/1000
-// let ok2 = new Date(final).toUTCString()
-
-// console.log(ok2)
-
-let date_changer = (date)=> {
-  let first = new Date(date).toUTCString()
-
-}
